462e3a98922ace8c4bbd14b6de8d87ea3810a3f6
ecmadao <wlec@outlook.com>
2016-06-30 13:08:11 +0800

    make threads

---
+++ threads/branch_thread.py
@@ -0,0 +1,24 @@
+#!usr/env/bin python
+import threading
+from time import sleep
+import random
+
+
+class BranchThread(threading.Thread):
+    __slots__ = ["branch_queue", "branch_spider"]
+
+    def __init__(self, branch_queue=None, branch_spider=None):
+        threading.Thread.__init__(self)
+        self.branch_queue = branch_queue
+        self.branch_spider = branch_spider
+
+    def run(self):
+        while 1:
+            url = self.branch_queue.get()
+            print('branch thread-{} start'.format(url))
+            branch_spider = self.branch_spider(url)
+            sleep(random.randrange(3, 10))
+            # TODO how to deal these response data?
+            result = branch_spider.request_page()
+            print('branch thread-{} end'.format(url))
+            self.branch_queue.task_done()
---
+++ threads/main_thread.py
@@ -0,0 +1,44 @@
+#!usr/env/bin python
+import threading
+from time import sleep
+import random
+import queue
+from .branch_thread import BranchThread
+
+existed_urls_list = []
+
+
+class MainThread(threading.Thread):
+    __slots__ = ['main_queue', 'main_spider', 'branch_spider']
+
+    def __init__(self, main_queue=None, main_spider=None, branch_spider=None):
+        threading.Thread.__init__(self)
+        self.main_queue = main_queue
+        self.main_spider = main_spider
+        self.branch_spider = branch_spider
+
+    def run(self):
+        global existed_urls_list
+        while 1:
+            url = self.main_queue.get()
+            print('main thread-{} start'.format(url))
+            main_spider = self.main_spider(url)
+            sleep(random.randrange(2, 5))
+            links = main_spider.request_urls()
+            branch_queue = queue.Queue(3)
+
+            for i in range(2):
+                branch_thread = BranchThread(branch_queue, self.branch_spider)
+                branch_thread.daemon = True
+                branch_thread.start()
+
+            for link in links:
+                if link not in existed_urls_list:
+                    existed_urls_list.append(link)
+                    branch_queue.put(link)
+
+            branch_queue.join()
+            print('main thread-{}\'s child threads is all finish'.format(url))
+            self.main_queue.task_done()
+
+
